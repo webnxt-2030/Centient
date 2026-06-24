ee2fdeb60aea5aa1e2bc97b00e5aefba04296207
Carl Emmanuel Macabales <156965373+cemmacabales@users.noreply.github.com>
2026-06-24 14:45:09 +0800

    fix: case sensitivity in SharedWalletError + add userIdentifierHistory to truncateAll

--- tests/helpers/db.ts
+++ tests/helpers/db.ts
@@ -33,6 +33,7 @@ export async function truncateAll(): Promise<void> {
   };
 
   await tryDelete(() => prisma.bannedIdentity.deleteMany());
+  await tryDelete(() => prisma.userIdentifierHistory.deleteMany());
   await tryDelete(() => prisma.balanceLedger.deleteMany());
   await tryDelete(() => prisma.campaignBalance.deleteMany());
   await tryDelete(() => prisma.submission.deleteMany());
